914f138db228277d4f25b557ab893a01b3fac69e
Paulina Żurek <paucik94@gmail.com>
2022-02-20 13:45:17 +0100

    Add for loop

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ shopping_list = {
     "piekarnia": ["chleb", "bułki", "pączek"],
     "warzywniak": ["marchew", "seler", "rukola"]
 }
+
+suma=0
+
+for shops, products in shopping_list.items():
+    
+    shops= shops.title()
+    products = [product.title() for product in products]
+    print(f"Idę do {shops} i kupuję tam {products}")
+    for product in products:
+       suma = suma + 1
+print(f"W sumie kupuję {suma} produktów")
